19799e3cfd822238643ef2440d54d303d408376e
Hrishikesh <hrskumar92@gmail.com>
2013-10-17 20:56:21 +0530

    first commit

---
+++ library/src/com/collegewires/api/calendar/Logr.java
@@ -0,0 +1,18 @@
+package com.collegewires.api.calendar;
+
+import android.util.Log;
+
+/** Log utility class to handle the log tag and DEBUG-only logging. */
+final class Logr {
+  public static void d(String message) {
+    if (BuildConfig.DEBUG) {
+      Log.d("CalendarSupportAPI", message);
+    }
+  }
+
+  public static void d(String message, Object... args) {
+    if (BuildConfig.DEBUG) {
+      d(String.format(message, args));
+    }
+  }
+}
