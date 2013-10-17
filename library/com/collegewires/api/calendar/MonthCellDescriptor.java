cc448d8b903def82e7aabc16734ef6f3bbbed0e8
Hrishikesh <hrskumar92@gmail.com>
2013-10-17 20:34:32 +0530

    first commit

---
+++ library/com/collegewires/api/calendar/MonthCellDescriptor.java
@@ -0,0 +1,116 @@
+package com.collegewires.api.calendar;
+
+/*
+ * Copyright 2013 Hrishikesh Kumar
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Based on https://github.com/square/android-times-square
+ * 
+ * Copyright 2012 Square, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.Date;
+
+/** Describes the state of a particular date cell in a {@link MonthView}. */
+class MonthCellDescriptor {
+  public enum RangeState {
+    NONE, FIRST, MIDDLE, LAST
+  }
+
+  private final Date date;
+  private final int value;
+  private final boolean isCurrentMonth;
+  private boolean isSelected;
+  private final boolean isToday;
+  private final boolean isSelectable;
+  private RangeState rangeState;
+
+  MonthCellDescriptor(Date date, boolean currentMonth, boolean selectable, boolean selected,
+      boolean today, int value, RangeState rangeState) {
+    this.date = date;
+    isCurrentMonth = currentMonth;
+    isSelectable = selectable;
+    isSelected = selected;
+    isToday = today;
+    this.value = value;
+    this.rangeState = rangeState;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public boolean isCurrentMonth() {
+    return isCurrentMonth;
+  }
+
+  public boolean isSelectable() {
+    return isSelectable;
+  }
+
+  public boolean isSelected() {
+    return isSelected;
+  }
+
+  public void setSelected(boolean selected) {
+    isSelected = selected;
+  }
+
+  public boolean isToday() {
+    return isToday;
+  }
+
+  public RangeState getRangeState() {
+    return rangeState;
+  }
+
+  public void setRangeState(RangeState rangeState) {
+    this.rangeState = rangeState;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  @Override public String toString() {
+    return "MonthCellDescriptor{"
+        + "date="
+        + date
+        + ", value="
+        + value
+        + ", isCurrentMonth="
+        + isCurrentMonth
+        + ", isSelected="
+        + isSelected
+        + ", isToday="
+        + isToday
+        + ", isSelectable="
+        + isSelectable
+        + ", rangeState="
+        + rangeState
+        + '}';
+  }
+}
